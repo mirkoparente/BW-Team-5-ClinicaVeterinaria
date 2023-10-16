c5486e1d91afdf521cbbc12d40a608abfff0f30a
LorenzoAluigi <134380077+LorenzoAluigi@users.noreply.github.com>
2023-10-16 13:17:26 +0200

    branch

---
+++ BW-Team-5-ClinicaVeterinaria/Controllers/UtentiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BW_Team_5_ClinicaVeterinaria.Controllers
+{
+    public class UtentiController : Controller
+    {
+        // GET: Utenti
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
