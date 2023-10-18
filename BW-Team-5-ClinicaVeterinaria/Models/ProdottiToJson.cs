f9ecd502f3db1bc284ef9959ff895bfb7dc075ac
Mirko Parente <133377396+mirkoparente@users.noreply.github.com>
2023-10-18 15:45:52 +0200

    async cerca prodotto farmacia

---
+++ BW-Team-5-ClinicaVeterinaria/Models/ProdottiToJson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BW_Team_5_ClinicaVeterinaria.Models
+{
+    public class ProdottiToJson
+    {
+        public int IdProdotti { get; set; }
+        public string Nome { get; set; }
+        public string NomeCassetto { get; set; }
+        public string NomeArmadietto { get; set; }
+
+        public string Descrizione { get; set; }
+
+
+    }
+}
